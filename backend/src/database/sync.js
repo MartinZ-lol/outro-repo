376870507c54d155da16804c5eadeeeac55437ee
Israel Martins <israelmartins92@outlook.com>
2024-11-06 12:03:10 -0300

    Modificando arquivos

--- backend/src/database/sync.js
+++ backend/src/database/sync.js
@@ -11,7 +11,7 @@ async function execute() {
     await CategoryModel.sync();
     await ProductCategoryModel.sync(); 
     await ProductOptionsModel.sync();
-    await UserModel.sync();  
+    await UserModel.sync({alter: true});  
 }
 
 execute();
